b991a3b70f82f175a8d9ea0d9e74c406e0d65054
esraaabdrabo <esraaabdrabo23@gmail.com>
2022-08-12 01:19:06 +0200

    create MVVM

--- lib/widgets.dart/detailsCard.dart
+++ lib/widgets.dart/detailsCard.dart
@@ -19,80 +19,83 @@ class _detailsCardState extends State<detailsCard> {
   bool needPaddingDetailsCard = false;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        print('click details');
-      },
-      child: Padding(
-        padding: EdgeInsets.only(
-            right: MediaQuery.of(context).size.width * .05,
-            left: MediaQuery.of(context).size.width * .05),
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: MyThemeData.accent,
-              boxShadow: [
-                BoxShadow(color: MyThemeData.detText, blurRadius: 5)
-              ]),
-          width: MediaQuery.of(context).size.width * .4,
-          child: Column(
-            children: [
-              Container(
-                child: Column(
-                  children: [
-                    noDetailsCard(
-                      widget.Movie,
-                      needPaddingDetailsCard,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          ////////rating//////////////
-
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.star,
-                                color: MyThemeData.text,
-                                size: 15,
-                              ),
-                              Padding(padding: EdgeInsets.only(left: 3.0)),
-                              Text(
-                                '${widget.rate.ceilToDouble()}',
-                                style: TextStyle(
-                                    color: MyThemeData.text, fontSize: 12),
-                              ),
-                            ],
-                          ),
-                          Padding(padding: EdgeInsets.all(5)),
-                          //////////////moviename////////////////////
-
-                          Text(
-                            '${widget.movieName}',
-                            style: TextStyle(
-                                color: MyThemeData.detText,
-                                letterSpacing: 2,
-                                fontSize: 10),
-                          ),
-                          Padding(padding: EdgeInsets.all(5)),
-                          ///////////year//////////////////////////
-
-                          Text(
-                            '${widget.year}',
-                            style: TextStyle(
-                                color: MyThemeData.detText,
-                                letterSpacing: 2,
-                                fontSize: 10),
-                          ),
-                        ],
+    return SizedBox(
+      //  height: MediaQuery.of(context).size.height * .2,
+      child: InkWell(
+        onTap: () {
+          print('click details');
+        },
+        child: Padding(
+          padding: EdgeInsets.only(
+              right: MediaQuery.of(context).size.width * .05,
+              left: MediaQuery.of(context).size.width * .05),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: MyThemeData.accent,
+                boxShadow: [
+                  BoxShadow(color: MyThemeData.detText, blurRadius: 5)
+                ]),
+            width: MediaQuery.of(context).size.width * .4,
+            child: Column(
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      noDetailsCard(
+                        widget.Movie,
+                        needPaddingDetailsCard,
                       ),
-                    )
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ////////rating//////////////
+
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  color: MyThemeData.text,
+                                  size: 15,
+                                ),
+                                Padding(padding: EdgeInsets.only(left: 3.0)),
+                                Text(
+                                  '${widget.rate.ceilToDouble()}',
+                                  style: TextStyle(
+                                      color: MyThemeData.text, fontSize: 12),
+                                ),
+                              ],
+                            ),
+                            Padding(padding: EdgeInsets.all(5)),
+                            //////////////moviename////////////////////
+
+                            Text(
+                              '${widget.movieName}',
+                              style: TextStyle(
+                                  color: MyThemeData.detText,
+                                  letterSpacing: 2,
+                                  fontSize: 10),
+                            ),
+                            Padding(padding: EdgeInsets.all(5)),
+                            ///////////year//////////////////////////
+
+                            Text(
+                              '${widget.year}',
+                              style: TextStyle(
+                                  color: MyThemeData.detText,
+                                  letterSpacing: 2,
+                                  fontSize: 10),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
